fdd04d22de28ae6947915b5d1cb31326e5c74d82
RabikovAAnd <randr.one@yandex.ru>
2025-04-29 10:56:36 +0500

    Добавьте файлы проекта.

---
+++ UserControl1.Designer.vb
@@ -0,0 +1,48 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class UserControl1
+    Inherits System.Windows.Forms.TextBox
+
+    'UserControl1 переопределяет метод Dispose для очистки списка компонентов.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Является обязательной для конструктора форм Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'Примечание: следующая процедура является обязательной для конструктора форм Windows Forms
+    'Для ее изменения используйте конструктор форм Windows Form.  
+    'Не изменяйте ее в редакторе исходного кода.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(163, 77)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 22)
+        Me.TextBox1.TabIndex = 0
+        '
+        'UserControl1
+        '
+        'Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        'Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        'Me.Controls.Add(Me.TextBox1)
+        'Me.Name = "UserControl1"
+        'Me.Size = New System.Drawing.Size(800, 450)
+        'Me.ResumeLayout(False)
+        'Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+End Class
---
+++ UserControl1.vb
@@ -0,0 +1,69 @@
+﻿Public Class UserControl1
+
+    ' Константы для сообщений Windows
+    Public Const WM_CHAR As Integer = &H102
+    Public Const WM_RBUTTONDOWN As Integer = &H204
+    Public Const WM_PASTE As Integer = &H302
+    Public Const WM_COPY As Integer = &H301
+    Public Const WM_CUT As Integer = &H300
+
+    Protected Overrides Sub WndProc(ByRef m As Message)
+        Select Case m.Msg
+            Case WM_CHAR
+                ' Разрешаем только цифры (48-57) и BackSpace (8)
+                If (m.WParam.ToInt32() > 57 Or m.WParam.ToInt32() < 48) And m.WParam.ToInt32() <> 8 Then
+                    Return
+                End If
+
+            Case WM_RBUTTONDOWN
+                ' Запрещаем правую кнопку мыши
+                Return
+
+            Case WM_PASTE, WM_COPY, WM_CUT
+                ' Запрещаем вставку, копирование и вырезание
+                Return
+
+            Case Else
+                ' Для всех остальных сообщений - стандартная обработка
+        End Select
+
+        ' Передаем сообщение базовому классу
+        MyBase.WndProc(m)
+    End Sub
+
+    ' Переопределяем свойство Text для запрета изменения из кода
+    Public Overrides Property Text() As String
+        Get
+            Return MyBase.Text
+        End Get
+        Set(ByVal value As String)
+            ' Оставляем пустым, чтобы запретить установку значения из кода
+        End Set
+    End Property
+
+    '' Разрешаем изменение текста только через специальный метод
+    'Public Sub SetNumericText(ByVal value As String)
+    '    ' Проверяем, что вводимая строка содержит только цифры
+    '    For Each c As Char In value
+    '        If Not Char.IsDigit(c) Then
+    '            Return
+    '        End If
+    '    Next
+    '    MyBase.Text = value
+    'End Sub
+
+    '' Запрещаем контекстное меню
+    'Protected Overrides Sub OnKeyDown(e As KeyEventArgs)
+    '    ' Запрещаем Ctrl+C, Ctrl+V, Ctrl+X
+    '    If e.Control AndAlso (e.KeyCode = Keys.C OrElse e.KeyCode = Keys.V OrElse e.KeyCode = Keys.X) Then
+    '        e.SuppressKeyPress = True
+    '        Return
+    '    End If
+    '    MyBase.OnKeyDown(e)
+    'End Sub
+
+    '' Запрещаем вставку через Drag&Drop
+    'Protected Overrides Sub OnDragEnter(e As DragEventArgs)
+    '    e.Effect = DragDropEffects.None
+    'End Sub
+End Class
